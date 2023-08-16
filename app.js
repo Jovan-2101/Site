61176c3fed26480a21aab4033ebbb90c1bdf656c
Јован <81017518+Jovan-2101@users.noreply.github.com>
2023-08-16 22:44:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,62 +7,13 @@ let NewFirst = true;
 
 let programskiJezici = [];
 
-function dodajZadatak(naziv, deskripcija, jezik, kod) {
-  const noviElement = {
-    naziv: naziv,
-    deskripcija: deskripcija,
-    jezik: jezik,
-    kod: kod
-  };
-
-  htmlSadrzaj.unshift(noviElement);
-}
-
-
-dodajZadatak(
-  "Novi zadatak",
-  "Ovo je opis novog zadatka",
-  "JavaScript",
-  `function 
-  noviZadatak() {\n  
-  // Implementacija novog zadatka\n"<}>"`
-);
-dodajZadatak(
-  "Novi zadatak",
-  "Ovo je opis novog zadatka",
-  "JavaScript",
-  `function noviZadatak() {\n  // Implementacija novog zadatka\n"<}>"`
-);
-
 function ucitajProgramskeJezike() {
   programskiJezici = [...new Set(htmlSadrzaj.map((zadatak) => zadatak.jezik))];
 }
 
 function escapeHTML(str) { 
   return str.replace(/</g, "&lt;").replace(/>/g, "&gt;");
-}
-
 function generisiBlok(i) {
-  const zadatak = htmlSadrzaj[i];
-  const div = document.createElement("div");
-  div.classList.add("blok", "animacija");
-
-  div.innerHTML = `
-    <h2>${zadatak.naziv}</h2>
-    <h4>${zadatak.deskripcija}</h4>
-    <pre>${zadatak.kod}</pre>
-    <button class="kopiraj-tekst">Kopiraj kod</button>
-  `;
-
-  const kopirajButton = div.querySelector(".kopiraj-tekst");
-  kopirajButton.onclick = function () {
-    kopirajKod(this);
-  };
-
-  zadaci.appendChild(div);
-
-  setTimeout(() => {
-    div.classList.add("active");
   }, 10 * (i + 1));
 }
 
@@ -83,70 +34,7 @@ function generisiSelect() {
 function kopirajKod(buttonElement) {
   const preElement = buttonElement.previousElementSibling;
   const range = document.createRange();
-  range.selectNodeContents(preElement);
-  const selection = window.getSelection();
-  selection.removeAllRanges();
-  selection.addRange(range);
-
-  try {
-    document.execCommand('copy');
-    const originalText = buttonElement.textContent;
-    buttonElement.textContent = 'Kopirano';
-    setTimeout(() => {
-      buttonElement.textContent = originalText;
-    }, 2000);
-  } 
-  catch (error) {
-    alert("Greška prilikom kopiranja koda.");
-  }
-  
-  selection.removeAllRanges();
-}
-
-
-function generisi() {
-  zadaci.innerHTML = '';
-  const searchInput = document.getElementById('search').value.toLowerCase().trim();
-  const selectedLanguage = document.getElementById('selectJezik').value;
-  
-  function filtrirajZadatke(i){
-   if ((selectedLanguage === 'svi' || htmlSadrzaj[i].jezik === selectedLanguage) &&
-        htmlSadrzaj[i].naziv.toLowerCase().includes(searchInput)) {
-        generisiBlok(i);
-      }
-  }
-  
-  if (NewFirst){
-    for (let i = 0; i < htmlSadrzaj.length; i++){
-     filtrirajZadatke(i);
-    }
-  }
-   else{
-    for (let i = htmlSadrzaj.length - 1; i >= 0; i--){
-     filtrirajZadatke(i);
-    }
-  }
-}
-
-
-
-function filtrirajPoNaslovu() {
-  generisi();
-}
-
-function filtrirajPoJeziku() {
-  const selectJezik = document.getElementById("selectJezik");
-  zadaci.innerHTML = '';
-  
-  if (selectJezik.value == "svi") {   
-    generisi();
-  } 
-  else {
-    for (let i = 0; i < htmlSadrzaj.length; i++) {
-      if (htmlSadrzaj[i].jezik == selectJezik.value) {
-        generisiBlok(i);
-      }
-    }
+   function filtrirajPoJeziku() {
   }
 }
 
@@ -234,9 +122,6 @@ function prikazJezika(){
 }
 
 function main(){ //moze da sluzi za restart
- htmlSadrzaj.forEach((zadatak) => { // zbog znakova'<' i '>'
-  zadatak.kod = escapeHTML(zadatak.kod);
- });
  sortDugmeUpdate();
  ucitajProgramskeJezike();
  generisiSelect();
@@ -246,5 +131,3 @@ function main(){ //moze da sluzi za restart
 }
 
 main();
-
-
